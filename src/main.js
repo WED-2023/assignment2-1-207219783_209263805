506475ec799aad9b6ffa2023a806075c08b4d550
noama11 <127957973+noama11@users.noreply.github.com>
2024-06-03 17:16:20 +0300

    noam first commit login page

--- src/main.js
+++ src/main.js
@@ -40,11 +40,11 @@ import {
 Vue.use(Vuelidate);
 
 axios.interceptors.request.use(
-  function(config) {
+  function (config) {
     // Do something before request is sent
     return config;
   },
-  function(error) {
+  function (error) {
     // Do something with request error
     return Promise.reject(error);
   }
@@ -52,11 +52,11 @@ axios.interceptors.request.use(
 
 // Add a response interceptor
 axios.interceptors.response.use(
-  function(response) {
+  function (response) {
     // Do something with response data
     return response;
   },
-  function(error) {
+  function (error) {
     // Do something with response error
     return Promise.reject(error);
   }
